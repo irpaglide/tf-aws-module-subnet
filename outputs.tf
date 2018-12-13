f78174afaf659e401f11d149d316de8997b547d7
juan.garcia <juan.garcia@waterworks.com>
2018-12-13 12:48:18 -0300

    fix private_route_table_id

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "private_subnet_ids" {
 }
 
 output "private_route_table_id" {
-  value = "${aws_route_table.private.id}"
+  value = "${aws_route_table.private.*.id}"
 }
 output "default_security_group_id" {
   value = "${aws_security_group.default.id}"
