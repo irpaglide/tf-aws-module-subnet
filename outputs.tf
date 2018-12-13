a601eca88debff3e4e2dcd79687d2f31f3cdaf69
juan.garcia <juan.garcia@waterworks.com>
2018-12-13 11:44:21 -0300

    output count

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,11 @@ output "private_subnet_ids" {
   value = ["${aws_subnet.private.*.id}"]
 }
 
+output "private_subnet_ids_count" {
+  value = "${length(aws_subnet.private)}"
+}
+
+
 output "default_security_group_id" {
   value = "${aws_security_group.default.id}"
 }
